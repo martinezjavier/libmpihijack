083f6052a0a7897bf116a6db0e7d231e98b6cb52
Javier Martinez Canillas <martinez.javier@gmail.com>
2011-03-15 01:40:24 +0100

    Better license documentation

--- libmpihijack.c
+++ libmpihijack.c
@@ -1,12 +1,21 @@
 /*
- * 	libhijack.c -- function interposition library for MPI primitives
+ * libhijack.c -- function interposition library for MPI primitives
  *
- * 	Author: Javier Martinez Canillas <martinez.javier@gmail.com>
+ * Author: Javier Martinez Canillas <martinez.javier@gmail.com>
  *
- *      This program is free software; you can redistribute it and/or
- *      modify it under the terms of the GNU General Public License
- *      as published by the Free Software Foundation; either version
- *      2 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -16,10 +25,10 @@
 
 int MPI_Init(int *argc, char ***argv)
 {
-	static int (*real_MPI_Init)(int *, char ***) = NULL;
+	static int (*real_MPI_Init)(int *, char ***);
 	int result;
 
-        fprintf(stderr,"MPI_Init interposition invoked argc=%d argv=%p\n", *argc, argv);
+        fprintf(stderr,"MPI_Init argc=%d argv=%p\n", *argc, argv);
 
 	real_MPI_Init = dlsym(RTLD_NEXT, "MPI_Init");
 	
